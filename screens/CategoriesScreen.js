c933cd2fec0bb5b181b6207432955daea2f2a145
Muhamed Musa <macbookpro@MacBooks-MacBook-Pro.local>
2020-06-09 02:39:38 +0200

    Final

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,29 +1,26 @@
-import React from "react";
+import React from 'react';
 import {
-  View,
-  Text,
   FlatList,
   StyleSheet,
-  TouchableOpacity,
-} from "react-native";
-import { HeaderButtons, Item } from "react-navigation-header-buttons";
+} from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-import HeaderButton from "../components/HeaderButton";
-import { CATEGORIES } from "../data/dummy-data";
-import CategoryGridTile from "../components/CategoryGridTile";
+import HeaderButton from '../components/HeaderButton';
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
-const CategoriesScreen = (props) => {
-  const renderGridItem = (itemData) => {
+const CategoriesScreen = props => {
+  const renderGridItem = itemData => {
     return (
       <CategoryGridTile
         title={itemData.item.title}
         color={itemData.item.color}
         onSelect={() => {
           props.navigation.navigate({
-            routeName: "CategoryMeals",
+            routeName: 'CategoryMeals',
             params: {
-              categoryId: itemData.item.id,
-            },
+              categoryId: itemData.item.id
+            }
           });
         }}
       />
@@ -40,9 +37,9 @@ const CategoriesScreen = (props) => {
   );
 };
 
-CategoriesScreen.navigationOptions = (navData) => {
+CategoriesScreen.navigationOptions = navData => {
   return {
-    headerTitle: "Meal Categories",
+    headerTitle: 'Meal Categories',
     headerLeft: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
@@ -53,16 +50,16 @@ CategoriesScreen.navigationOptions = (navData) => {
           }}
         />
       </HeaderButtons>
-    ),
+    )
   };
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default CategoriesScreen;
